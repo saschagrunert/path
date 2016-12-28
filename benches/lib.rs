3267a8940b8a7305b4b9f36e599c2104aeae9232
Sascha Grunert <Sascha.Grunert@rohde-schwarz.com>
2016-12-28 18:16:46 +0100

    Fixed benchmark compilation

--- benches/lib.rs
+++ benches/lib.rs
@@ -8,7 +8,7 @@ use std::net::{IpAddr, Ipv4Addr};
 
 #[bench]
 fn single_connection(bencher: &mut Bencher) {
-    let mut path :Path<u8> = Path::new();
+    let mut path :Path<u8, u8> = Path::new();
     let identifier = Identifier::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
                                      1234,
                                      IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
